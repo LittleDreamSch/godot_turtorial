4343c1fbadfd3fcdf8a697bf65a293b958ac7f63
梦想的Windows <zhuguorui@itp.ac.cn>
2023-09-17 22:05:48 +0800

    正在制作敌人

---
+++ player/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@export var speed: int = 40;
+@onready var animations = $AnimationPlayer
+
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left","ui_right", "ui_up","ui_down");
+	velocity = moveDirection * speed;
+
+func updateAnimation():
+	if velocity.length() == 0:
+		animations.stop()
+	else:
+		var direction = "Down";
+		if velocity.x < 0: direction = "Left"
+		elif velocity.x > 0: direction = "Right"
+		elif velocity.y < 0: direction = "Up"
+		animations.play("walk" + direction);
+
+func handleCollision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		print_debug(collider.name)
+
+func _physics_process(delta):
+	handleInput();
+	move_and_slide();
+	handleCollision()
+	updateAnimation();
